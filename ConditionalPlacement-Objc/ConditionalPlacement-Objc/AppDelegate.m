625afd14ce13293d7cd8aca37d7a3fcefd1944a2
Julien Guanzon <julienguanzon@gmail.com>
2015-01-24 20:12:56 -0700

    Add leonard and karl comparison

--- ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
+++ ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
@@ -18,9 +18,26 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     // Insert code here...
+    int karl = 10;
+    int leonard = 7;
+    int karlPlace;
+    int leonardPlace;
     
+    karlPlace = karl > 7 ? 1 : 2;
+    leonardPlace = leonard > 7 ? 1 : 2;
     
-    return YES;
+    if (leonardPlace == 1 && karlPlace == 1) {
+        NSLog(@"Both teachers are placed 1");
+    } else if (karlPlace == 1) {
+        NSLog(@"Karl is placed 1 and Leonard is is placed 2");
+    } else if (leonardPlace == 1) {
+        NSLog(@"Leonard is placed 1 and Karls is placed 2");
+    } else if (leonardPlace != 1) {
+        NSLog(@"Karl and Leonard are placed 2");
+    }
+return YES;
 }
+
+
 
 @end
